bb83437cde43e0b6ce53dfe5cc8a7917202bad36
jowel007 <abuhanifjowel1@gmail.com>
2024-05-21 11:28:10 +0600

    create admin role

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin/index');
 });
+
+Route::get('/createCustomer',[AuthController::class,'createCustomer']);
